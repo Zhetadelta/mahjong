52ce5eb63632de8669904a19b7d2c446babd5fbb
Abyss Halley <sakimorihalley@gmail.com>
2026-09-20 14:40:37 -0400

    initial setup for running things

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "mahjong :)", version, about = "riichi :D")]
+struct Cli {
+    #[command(subcommand)]
+    subcommand: SubCli,
+}
+
+#[derive(Subcommand)]
+enum SubCli {
+    Server {port: u16},
+    Client {ip: String, port: u16},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.subcommand {
+        SubCli::Server { port } => {
+            println!("server time! port is {port}");
+        }, 
+        SubCli::Client {ip, port} => { 
+            todo!();
+        },
+    }
 }
